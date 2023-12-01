965d5f593d7900679226bb6adf187326f969d1a7
Kirill Klimkin <violinkk29@gmail.com>
2023-12-01 18:23:03 +0300

    Enemies can die

--- scenes/enemies/enemy1.gd
+++ scenes/enemies/enemy1.gd
@@ -2,15 +2,21 @@ extends CharacterBody2D
 
 signal enemy_shoot(position: Vector2)
 
+var health: int = 5
 var can_shoot: bool = true
+
+func die() -> void:
+	queue_free()
 
 func _on_reload_timeout() -> void:
 	can_shoot = true
 	
 func hit() -> void:
-	print('enemy took damage')
+	health -= Globals.damage
 
 func _process(_delta: float) -> void:
+	if(health == 0):
+		die()
 	if(can_shoot):
 		enemy_shoot.emit(position)
 		can_shoot = false
